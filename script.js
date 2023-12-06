f7878727d68a6945ca9e574a79feb14ba6dbd399
brakistad <brakistad@gmail.com>
2023-12-06 01:23:19 +0100

    added new site

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Smooth scroll for navigation links
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // More interactive JS can be added here as needed
+});
