e64ef14824ca033d5bd932a7c32852b73a49e89e
brakistad <brakistad@gmail.com>
2023-12-09 00:37:33 +0000

    updated white invert on bylogo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,6 +75,7 @@ export default function Home() {
             width={100}
             height={24}
             priority
+            className="relative z-10 dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           />
         </a>
       </footer>
