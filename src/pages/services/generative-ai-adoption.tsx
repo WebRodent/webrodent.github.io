c66ed519d881b7ecae8b5bfdb75be04697696f23
brakistad <brakistad@gmail.com>
2023-12-11 17:56:09 +0000

    updated content cards

--- src/pages/services/generative-ai-adoption.tsx
+++ src/pages/services/generative-ai-adoption.tsx
@@ -1,6 +1,20 @@
 import Image from 'next/image'
 import '../../app/globals.css'
 
+type ContentCardProps = {
+    title: string;
+    content: string;
+  };
+  
+  const ContentCard: React.FC<ContentCardProps> = ({ title, content }) => (
+    <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card bg-gray shadow-md p-6 rounded-lg">
+      <div className="flex-1">
+        <h2 className="text-3xl font-bold text-white-500">{title}</h2>
+        <p className="mt-4 text-blue-200 text-lg leading-relaxed">{content}</p>
+      </div>
+    </div>
+);
+
 export default function GenerativeAIAdoption() {
       
     return (
@@ -13,36 +27,26 @@ export default function GenerativeAIAdoption() {
 
         {/* Content container */}
         <div className="w-full max-w-5xl flex flex-col gap-12 py-6">
-            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
-                <div className="flex-1">
-                    <h2 className="text-2xl font-bold">The Rise of Generative AI</h2>
-                    <p className="mt-4">Generative AI is revolutionizing technology, enabling machines to create new, original content from existing data. From producing realistic images and videos to generating human-like text, these AI models are pushing the boundaries of creativity and innovation. In this exploration, we delve into how Generative AI is being integrated into various industries, reshaping the future of automation and creativity.</p>
-                </div>
-            </div>
-            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
-                <div className="flex-1">
-                    <h2 className="text-2xl font-bold">Integrating AI into Business</h2>
-                    <p className="mt-4">Incorporating AI into your business strategy is not just about the technology: it&apos;s about reimagining processes and customer experiences. This article guides you through the essential steps and considerations for a successful AI integration, focusing on scalable, modular solutions that grow with your business needs.</p>
-                </div>
-            </div>
-            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
-                <div className="flex-1">
-                    <h2 className="text-2xl font-bold">Modular Architecture in AI Systems</h2>
-                    <p className="mt-4">Embracing a modular architecture in AI systems is crucial for scalability and flexibility. This article discusses how modular design allows for easier updates, maintenance, and customization, ensuring your AI solutions remain cutting-edge and adaptable to ever-evolving business needs.</p>
-                </div>
-            </div>
-            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
-                <div className="flex-1">
-                    <h2 className="text-2xl font-bold">AI-Driven Document Generation</h2>
-                    <p className="mt-4">Discover how AI is transforming document generation and approval processes. From drafting legal documents to creating detailed financial reports, AI-driven document generation accelerates workflows, minimizes errors, and offers new levels of efficiency and precision.</p>
-                </div>
-            </div>
-            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
-                <div className="flex-1">
-                    <h2 className="text-2xl font-bold">Fostering a Culture of Innovation with AI</h2>
-                    <p className="mt-4">Building a culture of innovation is essential in the AI era. Learn how encouraging creativity, continuous learning, and knowledge sharing within your teams can lead to groundbreaking AI solutions and a sustainable competitive edge in the marketplace.</p>
-                </div>
-            </div>
+        <ContentCard
+            title="The Rise of Generative AI"
+            content="Generative AI is revolutionizing technology, enabling machines to create new, original content from existing data. From producing realistic images and videos to generating human-like text, these AI models are pushing the boundaries of creativity and innovation. In this exploration, we delve into how Generative AI is being integrated into various industries, reshaping the future of automation and creativity."
+        />
+        <ContentCard
+            title="Integrating AI into Business"
+            content="Incorporating AI into your business strategy is not just about the technology: it's about reimagining processes and customer experiences. This article guides you through the essential steps and considerations for a successful AI integration, focusing on scalable, modular solutions that grow with your business needs."
+        />
+        <ContentCard
+            title="Modular Architecture in AI Systems"
+            content="Embracing a modular architecture in AI systems is crucial for scalability and flexibility. This article discusses how modular design allows for easier updates, maintenance, and customization, ensuring your AI solutions remain cutting-edge and adaptable to ever-evolving business needs."
+        />
+        <ContentCard
+            title="AI-Driven Document Generation"
+            content="Discover how AI is transforming document generation and approval processes. From drafting legal documents to creating detailed financial reports, AI-driven document generation accelerates workflows, minimizes errors, and offers new levels of efficiency and precision."
+        />
+        <ContentCard
+            title="Fostering a Culture of Innovation with AI"
+            content="Building a culture of innovation is essential in the AI era. Learn how encouraging creativity, continuous learning, and knowledge sharing within your teams can lead to groundbreaking AI solutions and a sustainable competitive edge in the marketplace."
+        />
         </div>
 
         
