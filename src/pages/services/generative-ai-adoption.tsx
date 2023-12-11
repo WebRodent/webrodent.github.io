abe297f82896c7db8e9459f705bd95472d904907
brakistad <brakistad@gmail.com>
2023-12-11 17:45:08 +0000

    added contents directly

--- src/pages/services/generative-ai-adoption.tsx
+++ src/pages/services/generative-ai-adoption.tsx
@@ -1,23 +1,8 @@
 import Image from 'next/image'
 import '../../app/globals.css'
-import ContentRow from '../../app/contents'; // Adjust the path as needed
 
 export default function GenerativeAIAdoption() {
-
-    const contents = [
-        {
-          title: 'The Rise of Generative AI',
-          article: 'Generative AI is changing the landscape of tech...',
-          imagePath: '/images/generative_ai_illustration.svg',
-        },
-        {
-          title: 'Integrating AI into Business',
-          article: 'When introducing AI into your business, there are many factors to consider...',
-          imagePath: null, // No image for this content row
-        },
-        // Add more content objects as needed
-      ];
-
+      
     return (
         <main className="flex flex-col items-center justify-top min-h-screen p-6">
         <header className="w-full max-w-5xl px-6 py-4 text-center">
@@ -27,16 +12,38 @@ export default function GenerativeAIAdoption() {
         </header>
 
         {/* Content container */}
-      <div className="w-full max-w-5xl flex flex-col gap-12 py-6">
-        {contents.map((content, index) => (
-          <ContentRow
-            key={index}
-            title={content.title}
-            article={content.article}
-            imagePath={content.imagePath}
-          />
-        ))}
-      </div>
+        <div className="w-full max-w-5xl flex flex-col gap-12 py-6">
+            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
+                <div className="flex-1">
+                    <h2 className="text-2xl font-bold">The Rise of Generative AI</h2>
+                    <p className="mt-4">Generative AI is revolutionizing technology, enabling machines to create new, original content from existing data. From producing realistic images and videos to generating human-like text, these AI models are pushing the boundaries of creativity and innovation. In this exploration, we delve into how Generative AI is being integrated into various industries, reshaping the future of automation and creativity.</p>
+                </div>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
+                <div className="flex-1">
+                    <h2 className="text-2xl font-bold">Integrating AI into Business</h2>
+                    <p className="mt-4">Incorporating AI into your business strategy is not just about the technology: it&apos;s about reimagining processes and customer experiences. This article guides you through the essential steps and considerations for a successful AI integration, focusing on scalable, modular solutions that grow with your business needs.</p>
+                </div>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
+                <div className="flex-1">
+                    <h2 className="text-2xl font-bold">Modular Architecture in AI Systems</h2>
+                    <p className="mt-4">Embracing a modular architecture in AI systems is crucial for scalability and flexibility. This article discusses how modular design allows for easier updates, maintenance, and customization, ensuring your AI solutions remain cutting-edge and adaptable to ever-evolving business needs.</p>
+                </div>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
+                <div className="flex-1">
+                    <h2 className="text-2xl font-bold">AI-Driven Document Generation</h2>
+                    <p className="mt-4">Discover how AI is transforming document generation and approval processes. From drafting legal documents to creating detailed financial reports, AI-driven document generation accelerates workflows, minimizes errors, and offers new levels of efficiency and precision.</p>
+                </div>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-6 items-center lg:items-start content-card">
+                <div className="flex-1">
+                    <h2 className="text-2xl font-bold">Fostering a Culture of Innovation with AI</h2>
+                    <p className="mt-4">Building a culture of innovation is essential in the AI era. Learn how encouraging creativity, continuous learning, and knowledge sharing within your teams can lead to groundbreaking AI solutions and a sustainable competitive edge in the marketplace.</p>
+                </div>
+            </div>
+        </div>
 
         
         <footer className="flex flex-col items-center justify-bottom w-full max-w-5xl px-6 py-4 text-center mt-auto">
