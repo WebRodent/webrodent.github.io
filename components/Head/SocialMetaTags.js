d371deb00ade7e323d617b39b5834a34f7687b37
brakistad <brakistad@gmail.com>
2024-09-17 23:16:17 +0200

    Refactor SocialMetaTags component to update og:description meta tag

--- components/Head/SocialMetaTags.js
+++ components/Head/SocialMetaTags.js
@@ -4,7 +4,7 @@ const SocialMetaTags = () => {
       <>
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content="Webrodent" />
-        <meta property="og:description" content="Affordable cloud hosting solutions by Webrodent." />
+        <meta property="og:description" content="Webrodent provides high-quality, affordable cloud hosting solutions for developers and businesses looking to scale their operations. Our services are designed to simplify cloud adoption and help you achieve your goals efficiently." />
         <meta property="og:url" content="https://webrodent.com" />
         <meta property="og:image" content="https://webrodent.com/assets/Rodent_Web_logo.png" />
         <meta property="og:image:width" content="1200" />
