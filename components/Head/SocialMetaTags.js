27a794ce145730d61823b9167c9dc2bd8f374ac1
brakistad <brakistad@gmail.com>
2024-09-17 23:03:42 +0200

    Refactor SocialMetaTags component to update Open Graph and Twitter Card meta tags

--- components/Head/SocialMetaTags.js
+++ components/Head/SocialMetaTags.js
@@ -6,7 +6,7 @@ const SocialMetaTags = () => {
         <meta property="og:title" content="Webrodent" />
         <meta property="og:description" content="Affordable cloud hosting solutions by Webrodent." />
         <meta property="og:url" content="https://webrodent.com" />
-        <meta property="og:image" content="https://webrodent.com/path-to-your-image.png" />
+        <meta property="og:image" content="https://webrodent.com/assets/Rodent_Web_logo.png" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Webrodent" />
   
@@ -14,7 +14,7 @@ const SocialMetaTags = () => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Webrodent" />
         <meta name="twitter:description" content="Affordable cloud hosting solutions by Webrodent." />
-        <meta name="twitter:image" content="https://webrodent.com/path-to-your-image.png" />
+        <meta name="twitter:image" content="https://webrodent.com/assets/Rodent_Web_logo.png" />
         <meta name="twitter:site" content="@yourTwitterHandle" />
       </>
     );
