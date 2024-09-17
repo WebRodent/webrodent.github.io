1edee80d21d94ff375db39200fa39a2325b138ac
brakistad <brakistad@gmail.com>
2024-09-17 23:36:09 +0200

    Refactor SocialMetaTags component to update og:image and twitter:image meta tags

--- components/Head/SocialMetaTags.js
+++ components/Head/SocialMetaTags.js
@@ -6,7 +6,7 @@ const SocialMetaTags = () => {
         <meta property="og:title" content="Webrodent" />
         <meta property="og:description" content="Webrodent provides high-quality, affordable cloud hosting solutions for developers and businesses looking to scale their operations. Our services are designed to simplify cloud adoption and help you achieve your goals efficiently." />
         <meta property="og:url" content="https://webrodent.com" />
-        <meta property="og:image" content="https://webrodent.com/assets/Rodent_Web_logo.png" />
+        <meta property="og:image" content="https://webrodent.com/assets/head_tag_image.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:type" content="website" />
@@ -15,8 +15,8 @@ const SocialMetaTags = () => {
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Webrodent" />
-        <meta name="twitter:description" content="Affordable cloud hosting solutions by Webrodent." />
-        <meta name="twitter:image" content="https://webrodent.com/assets/Rodent_Web_logo.png" />
+        <meta name="twitter:description" content="Webrodent provides high-quality, affordable cloud hosting solutions for developers and businesses looking to scale their operations. Our services are designed to simplify cloud adoption and help you achieve your goals efficiently." />
+        <meta name="twitter:image" content="https://webrodent.com/assets/head_tag_image.png" />
       </>
     );
   };
