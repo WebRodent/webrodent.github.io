fe3b5b7bd55a33a7fd22d857abf00ebb0743223f
BraKistad <brakistad@gmail.com>
2025-08-08 13:28:49 +0000

    Add Google Analytics integration to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID
 
 export default function RootLayout({
   children,
@@ -12,6 +14,19 @@ export default function RootLayout({
     <html lang="no">
       <body className={inter.className} suppressHydrationWarning>
         {children}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${googleAnalyticsId}');
+          `}
+        </Script>
       </body>
     </html>
   )
